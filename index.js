a3d7a1282a2abdecf28a6868212417ffecd95086
Guille Paz <guille87paz@gmail.com>
2013-05-05 01:18:38 -0300

    Create isNear function

--- index.js
+++ index.js
@@ -1,64 +1,72 @@
-(function (window) {
-    'use strict';
+/**
+ * Module dependencies
+ */
+var document = window.document,
+    on = window.addEventListener || window.attachEvent,
+    moveEvent = (window.attachEvent) ? 'onmousemove' : 'mousemove',
+    mousePoint = {};
 
-    /**
-     * Calculates the center of given element.
-     * @function
-     * @param {DOMElement} el - A DOM element to calculate its center.
-     * @returns {Object}
-     */
-    function calculateCenter(el) {
-        var clientRect = el.getBoundingClientRect();
+on(moveEvent, function (eve) {
 
-        return {
-            'x': clientRect.left + elA.clientHeight/2,
-            'y': clientRect.top + el.clientWidth/2
-        }
-    }
+    var coordX = 0,
+        coordY = 0;
 
-    /**
-     * Calculate distance between two points.
-     * @function
-     * @returns {Number}
-     */
-    function calculateDistance (a, b) {
-        return Math.floor(Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2)));
-    }
+    eve = eve || window.event;
 
-    /**
-     * Calculates if a given point is near to an element.
-     * @function
-     * @returns {Boolean}
-     */
-    function isNear(el, points, delta) {
-        delta = delta || 100;
-        var pointA = calculateCenter(el),
-            distance = calculateDistance(pointA, points),
-            isnear = false;
+    if (eve.pageX || eve.pageY) {
+        coordX = eve.pageX;
+        coordY = eve.pageY;
 
-        if (distance <= delta) {
-            isnear = true;
-        }
-
-        return isnear;
-    }
-
-    /**
-     * Expose isNear
-     */
-    // AMD suppport
-    if (typeof window.define === 'function' && window.define.amd !== undefined) {
-        window.define('isNear', [], function () {
-            return isNear;
-        });
-
-    // CommonJS suppport
-    } else if (typeof module !== 'undefined' && module.exports !== undefined) {
-        module.exports = isNear;
-
-    // Default
     } else {
-        window.isNear = isNear;
+        coordX = eve.clientX + doc.body.scrollLeft + docEl.scrollLeft;
+        coordY = eve.clientY + doc.body.scrollTop + docEl.scrollTop;
     }
 
-}(this));
+    mousePoint.x = coordX;
+    mousePoint.y = coordY;
+
+});
+
+/**
+ * Calculates if the mouse pointer position is near to a given element.
+ * @function
+ * @param {DOMElement} elementA - A given DOMElement.
+ * @param {DOMElement} elementB - A given DOMElement.
+ * @param {Number} delta - .
+ * @returns {Boolean}
+ */
+function isNear(element, delta) {
+
+    var clientRect = element.getBoundingClientRect(),
+        delta = delta || 100,
+        shadow = {
+            'top': clientRect.top - delta,
+            'right': clientRect.right + delta,
+            'bottom': clientRect.bottom + delta,
+            'left': clientRect.left - delta
+        },
+        near = false;
+
+    if ((mousePoint.x >= shadow.left && mousePoint.x <= shadow.right) && (mousePoint.y >= shadow.top && mousePoint.y <= shadow.bottom)) {
+
+        if ((mousePoint.x >= clientRect.left && mousePoint.x <= clientRect.right) && (mousePoint.y >= clientRect.top && mousePoint.y <= clientRect.bottom)) {
+
+            near = 'inside';
+
+        } else {
+
+            near = true;
+        }
+
+    } else {
+
+        near = false;
+    }
+
+    return near;
+}
+
+/**
+ * Expose isNear
+ */
+exports = module.exports = isNear;
